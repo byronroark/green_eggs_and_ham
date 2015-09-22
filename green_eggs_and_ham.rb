0ccaeddb2951276bb38da75ad109469e869c1c69
Byron Roark <byronroark@gmail.com>
2015-09-22 16:24:29 -0400

    first 2 tests passing

--- green_eggs_and_ham.rb
+++ green_eggs_and_ham.rb
@@ -3,4 +3,13 @@ class GreenEggsAndHam
   def initialize
     @text = File.read('green_eggs_and_ham.txt')
   end
+
+  def word_count
+    @text.split.count
+  end
+
+  def sorted_unique_words
+    words = @text.gsub(/[,.?!]/,"").split
+    words.map { |word| word.downcase }.uniq.sort
+  end
 end
